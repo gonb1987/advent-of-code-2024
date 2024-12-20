daaaf8b52d6a1fd3ed812c9be4c5c23bd724ce0b
Gonzalo Barrientos <gonzalo.barrientos@protonmail.com>
2024-12-20 07:54:18 +0100

    Day 17 completed

--- day_17/main.py
+++ day_17/main.py
@@ -1,4 +1,6 @@
 import re
+from heapq import heappush, heappop
+
 
 def get_input(filename):
     with open(filename) as f:
@@ -6,11 +8,15 @@ def get_input(filename):
     reg_a = int(re.search('Register A: (\d+)', raw_data).group(1))
     reg_b = int(re.search('Register B: (\d+)', raw_data).group(1))
     reg_c = int(re.search('Register C: (\d+)', raw_data).group(1))
-    program = list(map(int,re.search(r'Program: ((\d,?)+)',raw_data).group(1).split(',')))
+    program = list(map(int, re.search(r'Program: ((\d,?)+)', raw_data).group(1).split(',')))
     return reg_a, reg_b, reg_c, program
 
 
 class ChronoComputer:
+    """
+    Class that implements the computer logic and execute and reset methods to run the program
+    and to return the computer to the initial state.
+    """
     def __init__(self, reg_a, reg_b, reg_c, program):
         self.reg_a = reg_a
         self.reg_b = reg_b
@@ -19,12 +25,10 @@ class ChronoComputer:
         self.pointer = 0
         self.output = []
 
-
     def adv(self, operand):
         denominator = 2 ** self.combo(operand)
         self.reg_a = self.reg_a // denominator
         self.pointer += 2
-
 
     def bxl(self, operand):
         self.reg_b = self.reg_b ^ operand
@@ -34,36 +38,30 @@ class ChronoComputer:
         self.reg_b = self.combo(operand) % 8
         self.pointer += 2
 
-
     def jnz(self, operand):
         if self.reg_a == 0:
             self.pointer += 2
         else:
             self.pointer = operand
 
-
     def bxc(self, operand):
         self.reg_b = self.reg_b ^ self.reg_c
         self.pointer += 2
-
 
     def out(self, operand):
         self.output.append(self.combo(operand) % 8)
         self.pointer += 2
 
-
     def bdv(self, operand):
 
         denominator = 2 ** self.combo(operand)
-        self.reg_b = self.reg_b // denominator
+        self.reg_b = self.reg_a // denominator
         self.pointer += 2
-
 
     def cdv(self, operand):
         denominator = 2 ** self.combo(operand)
-        self.reg_c = self.reg_c // denominator
+        self.reg_c = self.reg_a // denominator
         self.pointer += 2
-
 
     def combo(self, operand):
         if 4 > operand >= 0:
@@ -96,21 +94,51 @@ class ChronoComputer:
             case 7:
                 return self.cdv
 
-
     def execute(self):
         while self.pointer < len(self.program) - 1:
             func = self.choose_func(self.program[self.pointer])
             operand = self.program[self.pointer + 1]
             func(operand)
 
+    def reset(self):
+        self.pointer = 0
+        self.output = []
+
+
+def match_program(computer):
+    """
+    Dijkstra's implementation for finding the smallest Register A that outputs the program given
+    octet by octet starting from the left of register A. The first octet corresponds to the last
+    output of the computer and that part of the output isn't modified when we add more octets.
+    .
+    :param computer:
+    :return: smallest register A that outputs the program given
+    """
+    avail_paths = [(bin(0), -1)]
+    while avail_paths:
+        a, index_to_match = heappop(avail_paths)
+        a = int(a, 2) << 3
+        for i in range(8):
+            computer.reset()
+            computer.reg_a = i + a
+            computer.execute()
+            if computer.output[0] == computer.program[index_to_match]:
+                if computer.output == computer.program:
+                    return a + i
+                heappush(avail_paths, (bin(a + i), index_to_match - 1))
+
+
 
 def main():
+    # Set up the computer
     reg_a, reg_b, reg_c, program = get_input('input_data')
     computer = ChronoComputer(reg_a, reg_b, reg_c, program)
+    # Run Part 1
     computer.execute()
     print(','.join(map(str, computer.output)))
+    # Run part 2
+    print(match_program(computer))
 
 
 if __name__ == '__main__':
     main()
-
--- day_17/test_main.py
+++ day_17/test_main.py
@@ -1,13 +1,89 @@
 from main import ChronoComputer
 import pytest
 
+
 @pytest.fixture
 def computer():
-    return ChronoComputer(0,0,0,[])
+    return ChronoComputer(0, 0, 0, [])
 
-@pytest.mark.parametrize("i,result", [(0, 0), (1,1), (2,2), (3,3), (4,10),(5,20),(6,30)])
+
+@pytest.mark.parametrize("i,result", [(0, 0), (1, 1), (2, 2), (3, 3), (4, 10), (5, 20), (6, 30)])
 def test_combo(computer, i, result):
     computer.reg_a = 10
     computer.reg_b = 20
     computer.reg_c = 30
     assert computer.combo(i) == result
+
+adv_program = [
+    (20, [0,2], 5),
+    (35, [0,4], 0),
+    (58, [0,3], 7),
+    (26, [0,5], 26)
+]
+
+@pytest.mark.parametrize('reg_a,program,result', adv_program)
+def test_adv(computer, reg_a, program, result):
+    computer.program = program
+    computer.reg_a = reg_a
+    computer.execute()
+    assert computer.reg_a == result
+
+bxl_program = [
+    (11, [1,2], 9),
+    (35, [1,4], 39),
+    (58, [1,3], 57),
+    (26, [1,5], 31)
+]
+
+@pytest.mark.parametrize('reg_b,program,result', bxl_program)
+def test_bxl(computer, reg_b, program, result):
+    computer.reg_b = reg_b
+    computer.program = program
+    computer.execute()
+    assert computer.reg_b == result
+
+
+bst_program = [
+    ([2,2], 2),
+    ([2,4], 4),
+    ([2,3], 3),
+    ([2,6], 1)
+]
+@pytest.mark.parametrize('program,result', bst_program)
+def test_bst(computer, program, result):
+    computer.reg_a = 100
+    computer.reg_b = 202
+    computer.reg_c = 305
+    computer.program = program
+    computer.execute()
+    assert computer.reg_b == result
+
+jnz_program = [
+    (0, 5, 2),
+    (1, 7, 7)
+]
+@pytest.mark.parametrize('reg_a,operand,result', jnz_program)
+def test_jnz(computer, reg_a, operand, result):
+    computer.reg_a = reg_a
+    computer.jnz(operand)
+    assert computer.pointer == result
+
+
+def test_bxc():
+    assert False
+
+
+def test_out():
+    assert False
+
+
+def test_bdv():
+    assert False
+
+
+def test_cdv():
+    assert False
+
+
+def test_choose_func():
+    assert False
